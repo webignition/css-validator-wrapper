aef772a03f34201799fa649264e32a70d127eaed
Jon Cram <jon@webignition.net>
2014-01-06 09:25:35 +0000

    Handle http server errors

--- tests/webignition/Tests/CssValidatorWrapper/Validate/HttpAuth/HttpServerErrorTest.php
+++ tests/webignition/Tests/CssValidatorWrapper/Validate/HttpAuth/HttpServerErrorTest.php
@@ -13,15 +13,75 @@ class HttpServerErrorTest extends BaseTest {
     
     
     public function testHttp500WhenRetrievingRootWebResource() {
-        $this->markTestSkipped('Not yet implemented');
+        $this->setHttpFixtures($this->getHttpFixtures($this->getFixturesDataPath(__FUNCTION__ . '/HttpResponses')));        
+        
+        $wrapper = $this->getNewCssValidatorWrapper();
+        
+        $wrapper->createConfiguration(array(         
+            'url-to-validate' => 'http://http-auth-01.simplytestable.com/',
+            'http-auth' => array(
+                'user' => 'example',
+                'password' => 'password'
+            )
+        ));
+        
+        $wrapper->setBaseRequest($this->getHttpClient()->get());
+        
+        /* @var $output \webignition\CssValidatorOutput\CssValidatorOutput */
+        $output = $wrapper->validate();
+        
+        $this->assertTrue($output->hasException());
+        $this->assertTrue($output->getException()->isHttp500());  
     }   
     
     public function testHttp500WhenRetrievingCssResourceOneOfTwo() {
-        $this->markTestSkipped('Not yet implemented');
+        $this->setHttpFixtures($this->getHttpFixtures($this->getFixturesDataPath(__FUNCTION__ . '/HttpResponses')));        
+        
+        $wrapper = $this->getNewCssValidatorWrapper(); 
+        $wrapper->setCssValidatorRawOutput($this->getFixture('CssValidatorResponse/1'));                
+
+        $wrapper->createConfiguration(array(          
+            'url-to-validate' => 'http://http-auth-01.simplytestable.com/',
+            'http-auth' => array(
+                'user' => 'example',
+                'password' => 'password'
+            )
+        ));
+        
+        $wrapper->setBaseRequest($this->getHttpClient()->get());
+        
+        /* @var $output \webignition\CssValidatorOutput\CssValidatorOutput */
+        $output = $wrapper->validate();
+        $errorsForExceptionedUrl = $output->getErrorsByUrl('http://http-auth-01.simplytestable.com/assets/css/style1.css');        
+        
+        $this->assertFalse($output->hasException());
+        $this->assertEquals(1, count($errorsForExceptionedUrl));
+        $this->assertEquals('http-error-500', $errorsForExceptionedUrl[0]->getMessage());
     }   
     
     public function testHttp500WhenRetrievingCssResourceTwoOfTwo() {
-        $this->markTestSkipped('Not yet implemented');
-    }
+        $this->setHttpFixtures($this->getHttpFixtures($this->getFixturesDataPath(__FUNCTION__ . '/HttpResponses')));        
+        
+        $wrapper = $this->getNewCssValidatorWrapper(); 
+        $wrapper->setCssValidatorRawOutput($this->getFixture('CssValidatorResponse/1'));        
+        
+        $wrapper->createConfiguration(array(           
+            'url-to-validate' => 'http://http-auth-01.simplytestable.com/',
+            'http-auth' => array(
+                'user' => 'example',
+                'password' => 'password'
+            )
+        ));
+        
+        $wrapper->setBaseRequest($this->getHttpClient()->get());
+        
+        /* @var $output \webignition\CssValidatorOutput\CssValidatorOutput */
+        $output = $wrapper->validate();
+        $errorsForExceptionedUrl = $output->getErrorsByUrl('http://http-auth-01.simplytestable.com/assets/css/style2.css');        
+        
+        $this->assertFalse($output->hasException());
+        $this->assertEquals(1, count($errorsForExceptionedUrl));
+        $this->assertEquals('http-error-500', $errorsForExceptionedUrl[0]->getMessage());
+    }   
     
 }
