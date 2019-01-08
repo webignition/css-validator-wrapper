0f7c5108b4c009fa4cba58e32d4597841b1d68e2
Jon Cram <jon@webignition.net>
2019-01-08 14:52:22 +0000

    Create CommandFactory

---
+++ src/CommandFactory.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace webignition\CssValidatorWrapper;
+
+use webignition\CssValidatorWrapper\Configuration\VendorExtensionSeverityLevel;
+
+class CommandFactory
+{
+    const JAVA_JAR_FLAG = '-jar';
+    const OUTPUT_FORMAT = 'ucn';
+
+    public function create(
+        string $url,
+        string $javaExecutablePath,
+        string $cssValidatorJarPath,
+        string $vendorExtensionSeverityLevel
+    ): string {
+        $options = [
+            'output' => self::OUTPUT_FORMAT,
+            'vextwarning' => VendorExtensionSeverityLevel::LEVEL_WARN === $vendorExtensionSeverityLevel
+                ? 'true'
+                : 'false'
+        ];
+
+        $commandParts = [
+            $javaExecutablePath,
+            self::JAVA_JAR_FLAG,
+            $cssValidatorJarPath,
+            $this->createOptionsString($options),
+            '"'.str_replace('"', '\"', $url).'"',
+            '2>&1'
+        ];
+
+        return implode(' ', $commandParts);
+    }
+
+    private function createOptionsString(array $options): string
+    {
+        $commandOptionsStrings = [];
+        foreach ($options as $key => $value) {
+            $commandOptionsStrings[] = '-'.$key.' '.$value;
+        }
+
+        return implode(' ', $commandOptionsStrings);
+    }
+}
