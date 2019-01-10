cdf02403e0c324c0f716310498d96e85095d31e8
Jon Cram <jon@webignition.net>
2019-01-10 12:55:51 +0000

    Create SourceInspector

---
+++ tests/SourceInspectorTest.php
@@ -0,0 +1,95 @@
+<?php
+/** @noinspection PhpUnhandledExceptionInspection */
+/** @noinspection PhpDocSignatureInspection */
+
+namespace webignition\CssValidatorWrapper\Tests\Wrapper;
+
+use Psr\Http\Message\UriInterface;
+use webignition\CssValidatorWrapper\SourceInspector;
+use webignition\CssValidatorWrapper\Tests\Factory\FixtureLoader;
+use webignition\WebPageInspector\WebPageInspector;
+use webignition\WebResource\WebPage\WebPage;
+use webignition\WebResourceInterfaces\WebPageInterface;
+
+class SourceInspectorTest extends \PHPUnit\Framework\TestCase
+{
+    /**
+     * @dataProvider findStylesheetUrlsDataProvider
+     */
+    public function testFindStylesheetUrls(WebPageInterface $webPage, array $expectedStylesheetUrls)
+    {
+        $webPageInspector = new WebPageInspector($webPage);
+
+        $sourceInspector = new SourceInspector($webPageInspector);
+
+        $this->assertEquals($expectedStylesheetUrls, $sourceInspector->findStylesheetUrls());
+    }
+
+    public function findStylesheetUrlsDataProvider()
+    {
+        return [
+            'no linked resources' => [
+                'webPage' => $this->createWebPage(
+                    FixtureLoader::load('Html/minimal-html5.html'),
+                    $this->createUri('http://example.com/')
+                ),
+                'expectedStylesheetUrls' => [],
+            ],
+            'single linked stylesheet' => [
+                'webPage' => $this->createWebPage(
+                    FixtureLoader::load('Html/minimal-html5-single-stylesheet.html'),
+                    $this->createUri('http://example.com/')
+                ),
+                'expectedStylesheetUrls' => [
+                    'http://example.com/style.css',
+                ],
+            ],
+            'three linked stylesheets' => [
+                'webPage' => $this->createWebPage(
+                    FixtureLoader::load('Html/minimal-html5-three-stylesheets.html'),
+                    $this->createUri('http://example.com/')
+                ),
+                'expectedStylesheetUrls' => [
+                    'http://example.com/one.css',
+                    'http://example.com/two.css',
+                    'http://example.com/three.css?foo=bar&foobar=foobar',
+                ],
+            ],
+            'foo' => [
+                'webPage' => $this->createWebPage(
+                    FixtureLoader::load('Html/minimal-html5-malformed-single-stylesheet.html'),
+                    $this->createUri('http://example.com/')
+                ),
+                'expectedStylesheetUrls' => [
+                    'http://example.com/style.css',
+                ],
+            ],
+        ];
+    }
+
+    private function createWebPage(string $content, UriInterface $uri): WebPageInterface
+    {
+        /* @var WebPage $webPage */
+        $webPage = WebPage::createFromContent($content);
+        $webPage = $webPage->setUri($uri);
+
+        return $webPage;
+    }
+
+    private function createUri(string $url): UriInterface
+    {
+        $uri = \Mockery::mock(UriInterface::class);
+        $uri
+            ->shouldReceive('__toString')
+            ->andReturn($url);
+
+        return $uri;
+    }
+
+    protected function tearDown()
+    {
+        parent::tearDown();
+
+        \Mockery::close();
+    }
+}
