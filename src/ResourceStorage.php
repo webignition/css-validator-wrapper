c5bf43229057959929f8cf90d8073163877dcd53
Jon Cram <webignition@gmail.com>
2019-01-10 15:03:10 +0000

    Add ResourceStorage::duplicate() (#54)

--- src/ResourceStorage.php
+++ src/ResourceStorage.php
@@ -8,18 +8,37 @@ class ResourceStorage
 
     public function store(string $content, string $type): string
     {
-        $path = sprintf(
-            '%s/%s.%s',
-            sys_get_temp_dir(),
-            md5($content . microtime(true)),
-            $type
-        );
+        return $this->persist($content, $type, function (string $path, string $content) {
+            file_put_contents($path, $content);
+        });
+    }
 
-        file_put_contents($path, $content);
+    public function duplicate(string $localPath, string $type): string
+    {
+        return $this->persist($localPath, $type, function (string $path, string $localPath) {
+            copy($localPath, $path);
+        });
+    }
+
+    private function persist(string $hashInput, string $type, callable $persister): string
+    {
+        $path = $this->createPath($hashInput, $type);
+
+        $persister($path, $hashInput);
 
         $this->paths[] = $path;
 
         return $path;
+    }
+
+    private function createPath(string $hashInput, string $type): string
+    {
+        return sprintf(
+            '%s/%s.%s',
+            sys_get_temp_dir(),
+            md5($hashInput . microtime(true)),
+            $type
+        );
     }
 
     public function deleteAll()
