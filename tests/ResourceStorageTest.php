07156294f83f74433e3caf3603f09d7383f46a7b
Jon Cram <jon@webignition.net>
2019-01-10 15:00:53 +0000

    Add ResourceStorage::duplicate()

--- tests/ResourceStorageTest.php
+++ tests/ResourceStorageTest.php
@@ -8,6 +8,8 @@ use webignition\CssValidatorWrapper\ResourceStorage;
 
 class ResourceStorageTest extends \PHPUnit\Framework\TestCase
 {
+    const MICROTIME = 123;
+
     /**
      * @dataProvider storeDataProvider
      */
@@ -19,17 +21,14 @@ class ResourceStorageTest extends \PHPUnit\Framework\TestCase
 
         $this->assertFalse(file_exists($expectedPath));
 
-        $microtime = 123;
-
-        $this->mockMicrotime($microtime);
-        $this->mockMd5($content . $microtime, $filenameHash);
+        $this->mockMicrotime(self::MICROTIME);
+        $this->mockMd5($content . self::MICROTIME, $filenameHash);
 
         $resourceStorage = new ResourceStorage();
 
         $path = $resourceStorage->store($content, $type);
 
-        $this->assertTrue(file_exists($expectedPath));
-        $this->assertEquals($content, file_get_contents($path));
+        $this->assertStoredFile($expectedPath, $path, $content);
 
         $resourceStorage->deleteAll();
 
@@ -52,6 +51,40 @@ class ResourceStorageTest extends \PHPUnit\Framework\TestCase
                 'expectedPath' => '/tmp/file-hash-2.css',
             ],
         ];
+    }
+
+    public function testDuplicate()
+    {
+        $content = '<!doctype html><html></html>';
+        $type = 'html';
+        $filenameHash = 'file-hash';
+        $expectedPath = '/tmp/file-hash.html';
+
+        $localPath = sys_get_temp_dir() . '/' . md5(microtime(true));
+
+        file_put_contents($localPath, $content);
+
+        $this->assertTrue(file_exists($localPath));
+
+        $this->mockMicrotime(self::MICROTIME);
+        $this->mockMd5($localPath . self::MICROTIME, $filenameHash);
+
+        $resourceStorage = new ResourceStorage();
+
+        $path = $resourceStorage->duplicate($localPath, $type);
+
+        $this->assertStoredFile($expectedPath, $path, $content);
+
+        $resourceStorage->deleteAll();
+
+        $this->assertFalse(file_exists($expectedPath));
+    }
+
+    private function assertStoredFile(string $expectedPath, string $path, string $expectedContent)
+    {
+        $this->assertSame($expectedPath, $path);
+        $this->assertTrue(file_exists($expectedPath));
+        $this->assertEquals($expectedContent, file_get_contents($path));
     }
 
     private function mockMicrotime(int $time)
