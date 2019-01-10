44688a40dfb3346a592536b579853655c10dca3e
Jon Cram <jon@webignition.net>
2019-01-10 15:38:42 +0000

    Add url arg to store() and duplicate()

--- src/ResourceStorage.php
+++ src/ResourceStorage.php
@@ -6,27 +6,27 @@ class ResourceStorage
 {
     private $paths = [];
 
-    public function store(string $content, string $type): string
+    public function store(string $url, string $content, string $type): string
     {
-        return $this->persist($content, $type, function (string $path, string $content) {
+        return $this->persist($url, $content, $type, function (string $path, string $content) {
             file_put_contents($path, $content);
         });
     }
 
-    public function duplicate(string $localPath, string $type): string
+    public function duplicate(string $url, string $localPath, string $type): string
     {
-        return $this->persist($localPath, $type, function (string $path, string $localPath) {
+        return $this->persist($url, $localPath, $type, function (string $path, string $localPath) {
             copy($localPath, $path);
         });
     }
 
-    private function persist(string $hashInput, string $type, callable $persister): string
+    private function persist(string $url, string $hashInput, string $type, callable $persister): string
     {
         $path = $this->createPath($hashInput, $type);
 
         $persister($path, $hashInput);
 
-        $this->paths[] = $path;
+        $this->paths[$url] = $path;
 
         return $path;
     }
--- tests/ResourceStorageTest.php
+++ tests/ResourceStorageTest.php
@@ -13,7 +13,7 @@ class ResourceStorageTest extends \PHPUnit\Framework\TestCase
     /**
      * @dataProvider storeDataProvider
      */
-    public function testStore(string $content, string $type, string $filenameHash, string $expectedPath)
+    public function testStore(string $url, string $content, string $type, string $filenameHash, string $expectedPath)
     {
         if (file_exists($expectedPath)) {
             unlink($expectedPath);
@@ -26,7 +26,7 @@ class ResourceStorageTest extends \PHPUnit\Framework\TestCase
 
         $resourceStorage = new ResourceStorage();
 
-        $path = $resourceStorage->store($content, $type);
+        $path = $resourceStorage->store($url, $content, $type);
 
         $this->assertStoredFile($expectedPath, $path, $content);
 
@@ -39,12 +39,14 @@ class ResourceStorageTest extends \PHPUnit\Framework\TestCase
     {
         return [
             'html file' => [
+                'url' => 'http://example.com/index.html',
                 'content' => '<!doctype html><html></html>',
                 'type' => 'html',
                 'filenameHash' => 'file-hash-1',
                 'expectedPath' => '/tmp/file-hash-1.html',
             ],
             'css file' => [
+                'url' => 'http://example.com/style.css',
                 'content' => 'html {}',
                 'type' => 'css',
                 'filenameHash' => 'file-hash-2',
@@ -55,6 +57,7 @@ class ResourceStorageTest extends \PHPUnit\Framework\TestCase
 
     public function testDuplicate()
     {
+        $url = 'http://example.com/index.html';
         $content = '<!doctype html><html></html>';
         $type = 'html';
         $filenameHash = 'file-hash';
@@ -71,7 +74,7 @@ class ResourceStorageTest extends \PHPUnit\Framework\TestCase
 
         $resourceStorage = new ResourceStorage();
 
-        $path = $resourceStorage->duplicate($localPath, $type);
+        $path = $resourceStorage->duplicate($url, $localPath, $type);
 
         $this->assertStoredFile($expectedPath, $path, $content);
 
