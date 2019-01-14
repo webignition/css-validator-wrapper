17f535826441188d067d97c4998b08b5ebe93519
Jon Cram <jon@webignition.net>
2019-01-14 17:31:43 +0000

    Create SourceHandler

---
+++ src/SourceHandler.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace webignition\CssValidatorWrapper;
+
+use webignition\WebResource\WebPage\WebPage;
+
+class SourceHandler
+{
+    private $webPage;
+    private $sourceMap;
+    private $inspector;
+    private $mutator;
+    private $preparer;
+
+    public function __construct(WebPage $webPage, SourceMap $sourceMap)
+    {
+        $this->webPage = $webPage;
+        $this->sourceMap = $sourceMap;
+
+        $this->inspector = new SourceInspector($webPage);
+        $this->mutator = new SourceMutator($webPage, $sourceMap, $this->inspector);
+        $this->preparer = new SourcePreparer($webPage, $sourceMap, $this->inspector);
+    }
+
+    public function getInspector(): SourceInspector
+    {
+        return $this->inspector;
+    }
+
+    public function getMutator(): SourceMutator
+    {
+        return $this->mutator;
+    }
+
+    public function getPreparer(): SourcePreparer
+    {
+        return $this->preparer;
+    }
+}
---
+++ tests/SourceHandlerTest.php
@@ -0,0 +1,31 @@
+<?php
+/** @noinspection PhpDocSignatureInspection */
+
+namespace webignition\CssValidatorWrapper\Tests\Wrapper;
+
+use webignition\CssValidatorWrapper\SourceHandler;
+use webignition\CssValidatorWrapper\SourceInspector;
+use webignition\CssValidatorWrapper\SourceMap;
+use webignition\CssValidatorWrapper\SourceMutator;
+use webignition\CssValidatorWrapper\SourcePreparer;
+use webignition\WebResource\WebPage\WebPage;
+
+class SourceHandlerTest extends \PHPUnit\Framework\TestCase
+{
+    public function testCreate()
+    {
+        /** @noinspection PhpUnhandledExceptionInspection */
+        $webPage = new WebPage();
+        $sourceMap = new SourceMap();
+
+        $sourceHandler = new SourceHandler($webPage, $sourceMap);
+
+        $this->assertInstanceOf(SourceInspector::class, $sourceHandler->getInspector());
+        $this->assertInstanceOf(SourceMutator::class, $sourceHandler->getMutator());
+        $this->assertInstanceOf(SourcePreparer::class, $sourceHandler->getPreparer());
+
+        $this->assertSame($webPage, $sourceHandler->getInspector()->getWebPage());
+        $this->assertSame($webPage, $sourceHandler->getMutator()->getWebPage());
+        $this->assertSame($webPage, $sourceHandler->getPreparer()->getWebPage());
+    }
+}
