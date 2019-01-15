c997169f4e7979b8d05a9b89c0b164995a8873ec
Jon Cram <jon@webignition.net>
2019-01-15 10:36:00 +0000

    Implement ArrayAccess on SourceMap

--- src/SourceMap.php
+++ src/SourceMap.php
@@ -2,24 +2,55 @@
 
 namespace webignition\CssValidatorWrapper;
 
-class SourceMap
+class SourceMap implements \ArrayAccess
 {
     private $mappings = [];
 
     public function __construct(array $mappings = [])
     {
         foreach ($mappings as $sourcePath => $localPath) {
-            $this->addMapping($sourcePath, $localPath);
+            $this[$sourcePath] = $localPath;
         }
-    }
-
-    public function addMapping(string $sourcePath, string $localPath)
-    {
-        $this->mappings[$sourcePath] = $localPath;
     }
 
     public function getLocalPath(string $sourcePath): ?string
     {
         return $this->mappings[$sourcePath] ?? null;
+    }
+
+    public function offsetExists($offset): bool
+    {
+        if (!is_string($offset)) {
+            return false;
+        }
+
+        return isset($this->mappings[$offset]);
+    }
+
+    public function offsetGet($offset): ?string
+    {
+        return $this->mappings[$offset] ?? null;
+    }
+
+    /**
+     * @param string $offset
+     * @param string $value
+     */
+    public function offsetSet($offset, $value)
+    {
+        if (!is_string($offset)) {
+            throw new \InvalidArgumentException('array key must be a string');
+        }
+
+        if (!is_string($value)) {
+            throw new \InvalidArgumentException('array value must be a string');
+        }
+
+        $this->mappings[$offset] = $value;
+    }
+
+    public function offsetUnset($offset)
+    {
+        unset($this->mappings[$offset]);
     }
 }
--- tests/SourceMapTest.php
+++ tests/SourceMapTest.php
@@ -41,4 +41,68 @@ class SourceMapTest extends \PHPUnit\Framework\TestCase
             ],
         ];
     }
+
+    public function testOffsetSetInvalidOffsetType()
+    {
+        $sourceMap = new SourceMap();
+
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('array key must be a string');
+
+        $sourceMap[] = 'foo';
+    }
+
+    public function testOffsetSetInvalidValueType()
+    {
+        $sourceMap = new SourceMap();
+
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('array value must be a string');
+
+        $sourceMap['foo'] = true;
+    }
+
+    public function testOffsetGet()
+    {
+        $mappings = [
+            'a' => 'first',
+            'b' => 'second',
+        ];
+        $sourceMap = new SourceMap($mappings);
+
+        $this->assertEquals('first', $sourceMap['a']);
+        $this->assertEquals('second', $sourceMap['b']);
+        $this->assertNull($sourceMap[1]);
+    }
+
+    public function testOffsetExists()
+    {
+        $mappings = [
+            'a' => 'first',
+            'b' => 'second',
+        ];
+        $sourceMap = new SourceMap($mappings);
+
+        $this->assertTrue(isset($sourceMap['a']));
+        $this->assertTrue(isset($sourceMap['b']));
+        $this->assertFalse(isset($sourceMap['c']));
+        $this->assertFalse(isset($sourceMap[1]));
+    }
+
+    public function testOffsetUnset()
+    {
+        $mappings = [
+            'a' => 'first',
+            'b' => 'second',
+        ];
+        $sourceMap = new SourceMap($mappings);
+
+        $this->assertEquals('first', $sourceMap['a']);
+        $this->assertEquals('second', $sourceMap['b']);
+
+        unset($sourceMap['a']);
+
+        $this->assertNull($sourceMap['a']);
+        $this->assertEquals('second', $sourceMap['b']);
+    }
 }
