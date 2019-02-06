c8cde0493d1e2070d0d855ad7a07224e4c373472
Jon Cram <jon@webignition.net>
2019-02-06 15:31:46 +0000

    Expose valid vendor extension severity levels

--- src/VendorExtensionSeverityLevel.php
+++ src/VendorExtensionSeverityLevel.php
@@ -7,4 +7,10 @@ class VendorExtensionSeverityLevel
     const LEVEL_IGNORE = 'ignore';
     const LEVEL_ERROR = 'error';
     const LEVEL_WARN = 'warn';
+
+    const VALID_VALUES = [
+        self::LEVEL_IGNORE,
+        self::LEVEL_WARN,
+        self::LEVEL_ERROR,
+    ];
 }
